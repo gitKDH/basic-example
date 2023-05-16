5ed1bfce70b66caab63b39260ce525cef2185f09
gitKDH <reg0212@naver.com>
2023-05-16 21:33:17 +0900

    05.16

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,16 +1,20 @@
 package hello.core.order;
 
 import hello.core.discount.Discountpolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private Discountpolicy discountpolicy;
+    private final MemberRepository memberRepository;
+    private final Discountpolicy discountpolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, Discountpolicy discountpolicy) {
+        this.memberRepository = memberRepository;
+        this.discountpolicy = discountpolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
